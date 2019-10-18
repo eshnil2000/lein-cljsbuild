9b779a65432d8d444d615e602524f9ef4f5ae5d6
vivaan <vivaan@dappsuni.com>
2019-10-18 23:36:24 +0000

    updated project.clj clojurescript version 1.10.520

--- example-projects/simple/project.clj
+++ example-projects/simple/project.clj
@@ -2,12 +2,11 @@
   :description "A simple example of how to use lein-cljsbuild"
   :source-paths ["src-clj"]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.521"
+                 [org.clojure/clojurescript "1.10.520"
                   :exclusions [org.apache.ant/ant]]
                  [compojure "1.1.6"]
                  [hiccup "1.0.4"]]
-  :plugins [[lein-cljsbuild "1.1.8-SNAPSHOT"]
-            [lein-ring "0.8.7"]]
+  :plugins [[lein-cljsbuild "1.1.7"] [lein-ring "0.8.7"]]
   :cljsbuild {
     :builds [{:source-paths ["src-cljs"]
               :compiler {:output-to "resources/public/js/main.js"
